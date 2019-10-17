2f2ca62b09c7f16b301817f0929d2181ba1f4469
YujiaXie <yujiaxie@dhcp-10-105-233-228.wireless.northwestern.private>
2019-10-16 23:54:16 -0500

    Updates

---
+++ precache-manifest.596578a51f9aed91d5fd8ed6d3d43cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22cf72f9ef231e88ec61b47349adba0",
+    "url": "/web101/index.html"
+  },
+  {
+    "revision": "97ac37a1c72c1005ec65",
+    "url": "/web101/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "28b25a2468cd0375c500",
+    "url": "/web101/static/js/2.394e7c18.chunk.js"
+  },
+  {
+    "revision": "97ac37a1c72c1005ec65",
+    "url": "/web101/static/js/main.87782863.chunk.js"
+  },
+  {
+    "revision": "191c5df4e868cc8ea984",
+    "url": "/web101/static/js/runtime-main.c6f5686b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/web101/static/media/logo.25bf045c.svg"
+  }
+]);
