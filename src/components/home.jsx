908c787a0f7d97d4f4506c3cf3416c6f7a5f63ab
Cynthia2019 <44718081+Cynthia2019@users.noreply.github.com>
2019-10-19 14:01:22 -0500

    Add files via upload

---
+++ src/components/home.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
+import './home.css'
+
+export default class HomePage extends Component{
+    render(){
+        return(
+            <div className='main-background'>
+            <Container>
+                <div className='avatar'>
+                    <Image src='https://via.placeholder.com/200' alt='avatar' roundedCircle></Image>
+                </div>
+                <div className='details'>
+                    <h1 className='name'>Yujia Xie</h1>
+                    <h2 className='main-details'>Undergraduate Student at Northwestern University</h2>
+                    <p className='extended-details'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A alias adipisci odio officiis odit voluptate qui dignissimos quidem, dolore sed ipsam magni vel quam illum ea. Dolore officia nihil maiores.</p>
+                </div>
+            </Container>
+            </div>
+        )
+    }
+}
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,1 +1,32 @@
+import React, {Component} from 'react';
+import Typist from 'react-typist';
+import './intro.css'
 
+export default class Intro extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            show: false 
+        };
+    }
+    render(){
+        const string ="incredible and amazing"
+        return(
+            <div className='section'>
+                <h1 className='headline'>
+                    {/* Static Text */}
+                    <span className='I-m'>
+                        Hi. I'm
+                    </span>
+                    {/* Automatically filled Text */}
+                    <span>
+                        <b className='fill-1'>
+                        <Typist cursor={{show: false, hideWhenDone: false}} avgTypingDelay={100}>{string}</Typist>
+                        </b>
+                    </span>
+                </h1>
+            </div>
+        )
+    }
+
+}
