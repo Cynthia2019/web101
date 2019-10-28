164101de277337e2869f39497dd3e8c20edc4dea
Cynthia2019 <ccynthia2019@gmail.com>
2019-10-27 22:06:02 -0500

    .

---
+++ precache-manifest.e8575bbbc36739b0a660f3ed695bb299.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25575ed06418d3d322d70508a04cb56",
+    "url": "/web101/index.html"
+  },
+  {
+    "revision": "ff5281a631def3d3742c",
+    "url": "/web101/static/css/2.0ab71c1a.chunk.css"
+  },
+  {
+    "revision": "b41510e41a3642af220f",
+    "url": "/web101/static/css/main.29d60b09.chunk.css"
+  },
+  {
+    "revision": "ff5281a631def3d3742c",
+    "url": "/web101/static/js/2.2a4024e1.chunk.js"
+  },
+  {
+    "revision": "b41510e41a3642af220f",
+    "url": "/web101/static/js/main.f33bd507.chunk.js"
+  },
+  {
+    "revision": "191c5df4e868cc8ea984",
+    "url": "/web101/static/js/runtime-main.c6f5686b.js"
+  },
+  {
+    "revision": "415a8c87d6bc8d888f72fba78f207fe7",
+    "url": "/web101/static/media/Batman.415a8c87.jpeg"
+  },
+  {
+    "revision": "b943785fcb454a93cd5f62a24368a577",
+    "url": "/web101/static/media/Generated_images10.b943785f.png"
+  },
+  {
+    "revision": "0cce8a5fb0b6b4cfdd9a3ea7f30201cd",
+    "url": "/web101/static/media/WechatIMG1.0cce8a5f.png"
+  },
+  {
+    "revision": "95ca9fe1f9e285446c72213d768dd210",
+    "url": "/web101/static/media/WechatIMG2.95ca9fe1.png"
+  },
+  {
+    "revision": "970e37ddb7de8c5ecd15064286b36a2f",
+    "url": "/web101/static/media/foxes.970e37dd.jpeg"
+  },
+  {
+    "revision": "8b4de0907de1b1a17a5f9d1ef2f93c09",
+    "url": "/web101/static/media/funnyAnimal.8b4de090.png"
+  },
+  {
+    "revision": "a78c3c766c7459624b20f01b3637fa89",
+    "url": "/web101/static/media/web-main-background.a78c3c76.jpeg"
+  }
+]);
